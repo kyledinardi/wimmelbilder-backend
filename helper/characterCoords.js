80d1d8a67e31afb14aecd3ac151a71f12d29a4eb
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-07-05 13:16:43 -0400

    Add route to validate coordinates

---
+++ helper/characterCoords.js
@@ -0,0 +1,17 @@
+module.exports = {
+  convention: {
+    Benson: { minX: 1588, maxX: 1655, minY: 410, maxY: 557 },
+    'Kermit the Frog': { minX: 2818, maxX: 2885, minY: 1831, maxY: 1991 },
+    'Waylon Smithers': { minX: 715, maxX: 791, minY: 1924, maxY: 2054 },
+  },
+  cyberpunkCity: {
+    Batman: { minX: 185, maxX: 255, minY: 964, maxY: 1037 },
+    'Jabba the Hutt': { minX: 1203, maxX: 1306, minY: 1067, maxY: 1164 },
+    'Tom Cat': { minX: 1683, maxX: 1757, minY: 3243, maxY: 3345 },
+  },
+  undergroundLab: {
+    Godzilla: { minX: 1501, maxX: 1642, minY: 1709, maxY: 1824 },
+    R2D2: { minX: 1411, maxX: 1455, minY: 2107, maxY: 2165 },
+    Waldo: { minX: 268, maxX: 307, minY: 1699, maxY: 1778 },
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const characterCoords = require('../helper/characterCoords');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get();
+router.post('/characters', (req, res, next) => {
+  const correctCoords =
+    characterCoords[req.body.illustration][req.body.character];
+
+  if (
+    req.body.selectedCoords.x >= correctCoords.minX &&
+    req.body.selectedCoords.x <= correctCoords.maxX &&
+    req.body.selectedCoords.y >= correctCoords.minY &&
+    req.body.selectedCoords.y <= correctCoords.maxY
+  ) {
+    res.json({ found: true });
+  } else {
+    res.json({ found: false });
+  }
+});
 
 module.exports = router;
