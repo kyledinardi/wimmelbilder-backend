9690f3be4e33322c349da7b0e858a5718e16e306
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-07-06 12:41:31 -0400

    Move character locations to database

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+const mongodb = process.env.MONGODB;
+
+async function main() {
+  await mongoose.connect(mongodb);
+}
+
+main().catch((err) => console.error(err));
 
 app.use(cors());
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
-const characterCoords = require('../helper/characterCoords');
+const asyncHandler = require('express-async-handler');
+const Character = require('../models/Character');
 
 const router = express.Router();
 
-router.post('/characters', (req, res, next) => {
-  const correctCoords =
-    characterCoords[req.body.illustration][req.body.character];
+router.post(
+  '/characters',
+  asyncHandler(async (req, res, next) => {
+    const character = await Character.findOne({
+      name: req.body.character,
+      illustration: req.body.illustration,
+    }).exec();
 
-  if (
-    req.body.selectedCoords.x >= correctCoords.minX &&
-    req.body.selectedCoords.x <= correctCoords.maxX &&
-    req.body.selectedCoords.y >= correctCoords.minY &&
-    req.body.selectedCoords.y <= correctCoords.maxY
-  ) {
-    res.json({ found: true });
-  } else {
-    res.json({ found: false });
-  }
-});
+    if (
+      req.body.selectedCoords.x >= character.minX &&
+      req.body.selectedCoords.x <= character.maxX &&
+      req.body.selectedCoords.y >= character.minY &&
+      req.body.selectedCoords.y <= character.maxY
+    ) {
+      res.json({ found: true });
+    } else {
+      res.json({ found: false });
+    }
+  }),
+);
 
 module.exports = router;
