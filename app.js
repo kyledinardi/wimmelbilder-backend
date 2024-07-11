0e64212da7a495cb6cad52914aff4ab7d461754f
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-07-11 12:08:37 -0400

    Push to production

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const debug = require('debug')('odin-wheres-waldo-backend:app');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 
 const indexRouter = require('./routes/index');
 
@@ -15,13 +19,21 @@ async function main() {
   await mongoose.connect(mongodb);
 }
 
-main().catch((err) => console.error(err));
+main().catch((err) => debug(err));
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+});
+
+app.use(limiter);
 app.use(cors());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
